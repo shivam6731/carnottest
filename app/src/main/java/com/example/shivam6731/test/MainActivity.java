ab4d38b934c17f784a308f5fe43a96d0eb1ab503
shivam <shivambajpai45@gmail.com>
2017-11-15 14:47:41 +0530

    added start end savestart and saveend timstamp

--- app/src/main/java/com/example/shivam6731/test/MainActivity.java
+++ app/src/main/java/com/example/shivam6731/test/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -28,20 +31,76 @@ public class MainActivity extends AppCompatActivity {
     databasehelper3 dd3=null;
     databasehelper4 dd4=null;
     int k=0;
+    TextView a1,a2,a3,a4,b1,b2,b3,b4,c1,c2,c3,c4,d1,d2,d3,d4;
+    String q1,q2,w1,w2,e1,e2,r1,r2;
+     long ss1,ss2,ss3,ss4,se1,se2,se3,se4;
+    Button btn1,btn2,btn3,btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=(Button)findViewById(R.id.btn1);
+        btn2=(Button)findViewById(R.id.btn2);
+        btn3=(Button)findViewById(R.id.btn3);
+        btn4=(Button)findViewById(R.id.btn4);
+
+        a1=(TextView)findViewById(R.id.a1);
+        a2=(TextView)findViewById(R.id.a2);
+        a3=(TextView)findViewById(R.id.a3);
+        a4=(TextView)findViewById(R.id.a4);
+        b1=(TextView)findViewById(R.id.b1);
+        b2=(TextView)findViewById(R.id.b2);
+        b3=(TextView)findViewById(R.id.b3);
+        b4=(TextView)findViewById(R.id.b4);
+        c1=(TextView)findViewById(R.id.c1);
+        c2=(TextView)findViewById(R.id.c2);
+        c3=(TextView)findViewById(R.id.c3);
+        c4=(TextView)findViewById(R.id.c4);
+        d1=(TextView)findViewById(R.id.d1);
+        d2=(TextView)findViewById(R.id.d2);
+        d3=(TextView)findViewById(R.id.d3);
+        d4=(TextView)findViewById(R.id.d4);
+
         dd1=new databasehelper1(this);
         dd2=new databasehelper2(this);
         dd3=new databasehelper3(this);
         dd4=new databasehelper4(this);
        //boolean x= pingUrl("https://jsonplaceholder.typicode.com/comments");
         new UpdateTask().execute();
+       // text();
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                k=1;
+                new UpdateTask().execute();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                k=2;
+                new UpdateTask().execute();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                k=3;
+                new UpdateTask().execute();
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                k=4;
+                new UpdateTask().execute();
+            }
+        });
 
     }
     private class UpdateTask extends AsyncTask<String, String,String> {
         protected String doInBackground(String... urls) {
+
             if(k==0)
             {
                 ping("https://jsonplaceholder.typicode.com/comments",1);
@@ -49,10 +108,28 @@ public class MainActivity extends AppCompatActivity {
                ping("https://jsonplaceholder.typicode.com/todos",3);
                 ping("https://jsonplaceholder.typicode.com/posts",4);
             }
+            else if(k==1)
+            {
+                ping("https://jsonplaceholder.typicode.com/comments",1);
+            }
+            else if(k==2)
+            {
+                ping("https://jsonplaceholder.typicode.com/photos",2);
+            }
+            else if(k==3)
+            {
+                ping("https://jsonplaceholder.typicode.com/todos",3);
+            }
+            else if(k==4)
+            {
+                ping("https://jsonplaceholder.typicode.com/posts",4);
+            }
+
 
 
             return null;
         }
+
 
     }
     void ping(String uri,int c)
@@ -65,9 +142,42 @@ public class MainActivity extends AppCompatActivity {
             urlConn.connect();
             final long endTime = System.currentTimeMillis();
             if (urlConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                System.out.println("Time (ms) : " + (endTime - startTime));
-                System.out.println("Ping to "+startTime+" "+endTime+"" +" was success");
-                getdata(uri,c);
+
+                if(c==1)
+                {
+                    q1=startTime+"";
+                    q2=endTime+"";
+                    ss1=System.currentTimeMillis();
+                    getdata(uri,c);
+                    se1=System.currentTimeMillis();
+                }
+                else  if(c==2)
+                {
+                    w1=startTime+"";
+                    w2=endTime+"";
+                    ss2=System.currentTimeMillis();
+                    getdata(uri,c);
+                    se2=System.currentTimeMillis();
+                }
+                else  if(c==3)
+                {
+                    e1=startTime+"";
+                    e2=endTime+"";
+                    ss3=System.currentTimeMillis();
+                    getdata(uri,c);
+                    se3=System.currentTimeMillis();
+                }
+                else  if(c==4)
+                {
+                    r1=startTime+"";
+                    r2=endTime+"";
+                    ss4=System.currentTimeMillis();
+                    getdata(uri,c);
+                    se4=System.currentTimeMillis();
+                }
+                System.out.println(q1+q2+w1+w2+e1+e2+r1+r2);
+                //System.out.println("Ping to "+startTime+" "+endTime+"" +" was success");
+
             }
         } catch (final MalformedURLException e1) {
             e1.printStackTrace();
@@ -155,26 +265,86 @@ public class MainActivity extends AppCompatActivity {
 
         //adding the string request to request queue
         requestQueue.add(stringRequest);
+
     }
 
     public void ADDDATA(String ii,String ii1,String ii2,String ii3,String ii4)
     {
 
         dd1.addData(ii,ii1,ii2,ii3,ii4);
+        text();
     }
     public void ADDDATA2(String ii,String ii1,String ii2,String ii3,String ii4)
     {
 
         dd2.addData(ii,ii1,ii2,ii3,ii4);
+        text();
     }
     public void ADDDATA3(String ii,String ii1,String ii2,String ii3)
     {
 
         dd3.addData(ii,ii1,ii2,ii3);
+        text();
     }
     public void ADDDATA4(String ii,String ii1,String ii2,String ii3)
     {
 
         dd4.addData(ii,ii1,ii2,ii3);
+        text();
+    }
+    void text()
+    {
+
+        if(k==0) {
+            a1.setText(q1 + "");
+            a2.setText(q2 + "");
+            a3.setText(ss1 + "");
+            a4.setText(se1 + "");
+
+            b1.setText(w1 + "");
+            b2.setText(w2 + "");
+            b3.setText(ss2 + "");
+            b4.setText(se2 + "");
+
+            c1.setText(e1 + "");
+            c2.setText(e2 + "");
+            c3.setText(ss3 + "");
+            c4.setText(se3 + "");
+
+
+            d1.setText(r1 + "");
+            d2.setText(r2 + "");
+            d3.setText(ss4 + "");
+            d4.setText(se4 + "");
+        }
+        else if(k==1)
+        {
+            a1.setText(q1 + "");
+            a2.setText(q2 + "");
+            a3.setText(ss1 + "");
+            a4.setText(se1 + "");
+        }
+        else if(k==2)
+        {
+            b1.setText(w1 + "");
+            b2.setText(w2 + "");
+            b3.setText(ss2 + "");
+            b4.setText(se2 + "");
+        }
+        else if(k==3)
+        {
+            c1.setText(e1 + "");
+            c2.setText(e2 + "");
+            c3.setText(ss3 + "");
+            c4.setText(se3 + "");
+        }
+        else if(k==4)
+        {
+            d1.setText(r1 + "");
+            d2.setText(r2 + "");
+            d3.setText(ss4 + "");
+            d4.setText(se4 + "");
+        }
+
     }
 }
